a608e3478da9354441d922e3ca0da6f23d05a6e1
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2020-01-03 23:01:14 +0300

    Adit about page with multilingual content from and styles

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,15 +1,55 @@
 import React from "react";
 import Header from '../Header';
+import ReactHtmlParser from "react-html-parser";
 
 import "./About.scss";
 
 
 export default class About extends React.Component {
+
+    getLangText(text) {
+        return ReactHtmlParser(this.props.text[text]);
+    }
+
     render() {
         return (
-            <div className="Blog">
+            <div className="About">
                 <Header text={this.props.text}/>
-                
+                    <div className="container">
+                        <div className="about-block">
+                            <h1>{this.getLangText("AboutPageFirstHeader")}</h1>
+                            <p>{this.getLangText("AboutPageFirstParagraph")}</p>
+                        </div>
+                        <div className="about-block">
+                            <h1>{this.getLangText("AboutPageSecondHeader")}</h1>
+                            <p>{this.getLangText("AboutPageSecondParagraph")}</p>
+                        </div>
+                        <div className="about-block">
+                            <h1>{this.getLangText("AboutPageThirdHeader")}</h1>
+                            <p>{this.getLangText("AboutPageThirdParagraph")}</p>
+                        </div>
+                        <div className="about-block">
+                            <h1>{this.getLangText("AboutPageFourthHeader")}</h1>
+                            <p>{this.getLangText("AboutPageFourthParagraph")}</p>
+                            <div className="about-buttons">
+                                <a 
+                                    className="about-button"
+                                    href="/download">
+                                    {this.getLangText("ForBeginners")}
+                                </a>
+                                <a 
+                                    className="about-button"
+                                    href="/download">
+                                    {this.getLangText("ForAverages")}
+                                </a>
+                                <a 
+                                    className="about-button"
+                                    href="/download">
+                                    {this.getLangText("ForProfessionals")}
+                                </a>
+                            </div>
+                        </div>
+                    </div>
             </div>
         );
     }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,9 +5,11 @@ import ReactHtmlParser from "react-html-parser";
 
 
 export default class Header extends React.Component {
+
     getLangText(text) {
         return ReactHtmlParser(this.props.text[text]);
     }
+    
     render() {
         return (
             <header className="Header">
