c9e0f5e10257129197695fb0f3288acfa4a43c69
pavladan <fatalfenker@gmail.com>
2020-03-25 13:02:22 +0300

    Multilang scrolled content and image

--- src/assets/images/products/scroll-pages/ExtensionImagesStatic.js
+++ src/assets/images/products/scroll-pages/ExtensionImagesStatic.js
@@ -1,60 +1,45 @@
-
-
 // Import images to description pages (that are using onScroll effect)
-import image_1 from '../extension/1.png';
-import image_2 from '../extension/2.png';
-import image_3 from '../extension/3.png';
-import image_4 from '../extension/4.png';
-import image_5 from '../extension/5.png';
-import image_6 from '../extension/6.png';
-import image_7 from '../extension/7.png';
-import image_8 from '../extension/8.png';
-import image_9 from '../extension/9.png';
-import image_10 from '../extension/10.png';
+import image_1 from "../extension/1.png";
+import image_2 from "../extension/2.png";
+import image_3 from "../extension/3.png";
+import image_4 from "../extension/4.png";
+import image_5 from "../extension/5.png";
+import image_6 from "../extension/6.png";
+import image_7 from "../extension/7.png";
+import image_8 from "../extension/8.png";
+import image_9 from "../extension/9.png";
+import image_10 from "../extension/10.png";
 
-
-//An array of objects to scroll (image src + coordinates to display)
+//An array of objects to scroll (image en + coordinates to display)
 export const extensionImages = [
-    {
-        src: image_1,
-    },
-	{
-        src: image_2,
-    },
-	{
-        src: image_3,
-    },
-	{
-        src: image_4,
-    },
-	{
-        src: image_5,
-    },
-	{
-        src: image_6,
-    },
-	{
-        src: image_7,
-    },
-	{
-        src: image_8,
-    },
-	{
-        src: image_9,
-    },
-	{
-        src: image_10,
-    }
+  {
+    en: image_1
+  },
+  {
+    en: image_2
+  },
+  {
+    en: image_3
+  },
+  {
+    en: image_4
+  },
+  {
+    en: image_5
+  },
+  {
+    en: image_6
+  },
+  {
+    en: image_7
+  },
+  {
+    en: image_8
+  },
+  {
+    en: image_9
+  },
+  {
+    en: image_10
+  }
 ];
-
-
-
-
-
-
-
-
-
-
-
-
